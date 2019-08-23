bf86bf5a57049d9081361885b737bc050678d8dc
Roger Wellard <rogerwellard@gmail.com>
2019-08-23 13:36:16 +0200

    updated maya menu creation

--- maya/menus/pc_maya_tools_menu.py
+++ maya/menus/pc_maya_tools_menu.py
@@ -1,34 +1,28 @@
 # CREATES A POLYCAT MENU IN MAYA
-# this is run as a script rather that a imported and run as a function.
-# POLYCAT 2019
+# !! This script is run with pm.evalDeferred() in the userSetup.py !!
+# POLYCAT ANIMATION
 
 import pymel.core as pm
+
 
 def executthis():
     test = "this is working"
     print (test)
 
+def createMayaMenus():
 
-# THIS SECTION FOR DEV PURPOSES SO THAT WE CAN CREATE AND DELETE THE MENU
-# main_window = pm.language.melGlobals["gMainWindow"]
-# menu_obj = "pcToolsMenu"
-# menu_label = "Polycat Tools"
-#
-# try:
-#     if pm.menu(menu_obj,label=menu_label,exists=True,parent=main_window):
-#         pm.delete(pm.menu(menu_obj,e=True,deleteAllItems=True))
-# except:
-#     print("there was an error deleting the menu")
+    main_window = pm.language.melGlobals["gMainWindow"]
+    menu_obj = "pcToolsMenu"
+    menu_label = "Polycat Tools"
 
+    # this starts building a heiracy. setParent automatically gets assigned to the parent of the last created menu.
 
+    custom_tools_menu = pm.menu(menu_obj, label=menu_label, parent=main_window, tearOff=True)
+    pm.menuItem(label="Scene",subMenu=True, parent=custom_tools_menu, tearOff=True)
+    pm.menuItem(label="Export Camera",command="executthis()")
 
-# this starts building a heiracy. setParent automatically gets assigned to the parent of the last created menu.
+    # sets the parent back to the specified menu
+    pm.setParent("..",menu=True)
 
-custom_tools_menu = pm.menu(menu_obj, label=menu_label, parent=main_window, tearOff=True)
-pm.menuItem(label="Scene",subMenu=True, parent=custom_tools_menu, tearOff=True)
-pm.menuItem(label="Export Camera",command="executthis()")
+    # add in other pm.menu and pm.menuItem after this to create more menus
 
-# sets the parent back to the specified menu
-pm.setParent("..",menu=True)
-
-# add in other pm.menu and pm.menuItem after this to create more menus
--- maya/userSetup.py
+++ maya/userSetup.py
@@ -1,15 +1,9 @@
-import sys
-import os
+# Polycat Animation
+# Standard user setup file for maya
 
-# opens the command port for pycharm
-# import maya.cmds as cmds
-#
-# if not cmds.commandPort(':4434', query=True):
-#     cmds.commandPort(name=':4434')
+import pymel.core as pm
 
-#START OF PIPELINE REQUIREMENTS
-# adds our script location for the maya session.
-script_path = os.getenv("MAYA_PIPELINE_LIBRARY")
-sys.path.append(script_path)
+#imports the script and runs the menu command
+pm.evalDeferred("import pc_maya_tools_menu;pc_maya_tools_menu.createMayaMenus()")
 
 
