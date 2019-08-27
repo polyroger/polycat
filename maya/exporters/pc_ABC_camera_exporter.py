8ccf75cd64976851cc2cf7e97e6cccfb1e4a002f
Roger Wellard <rogerwellard@gmail.com>
2019-08-27 12:21:31 +0200

    re wrote the script to run as a function call from the maya menu

--- maya/exporters/pc_ABC_camera_exporter.py
+++ maya/exporters/pc_ABC_camera_exporter.py
@@ -12,58 +12,58 @@ A houdini camera at scale 0.1 is exported as well as a maya camera at scale 1.
 import pymel.core as pm
 import os
 
-class MayaCamera():
 
-    def __init__(self):
-        self.selection = pm.ls(selection=True)
+class ExportMe():
 
-        if self.selection:
-            self.selection = self.selection[0]
-            self.shape = self.selection.getShape()
-        else:
-            print("you have not selected a camera")
+    def __init__(self,mydirname,mysavename,myext):
+        self.mydirname = mydirname
+        self.mysavename = mysavename
+        self.myext = myext
 
-    def export_me(self,mayadirname,mayasavename,mayaext):
+    def export(self, appname):
 
         # sets the string arguments needed for the ABC export a
-        myfile = str("-file " + mayadirname + "/" + mayasavename + "_maya" + mayaext)
+        myfile = str("-file " + self.mydirname + "/" + self.mysavename + "_" + appname + self.myext)
         root = str("-root " + str(self.selection))
         myframerange = "-frameRange " + str(start) + " " + str(end)
         exportcommand = myfile + " " + root + " " + myframerange + " " + "-eulerFilter -worldspace"
         pm.AbcExport(j=exportcommand)
 
-    def __str__(self):
-        return ("{}".format(self.selection))
-
-
-class HoudiniCamera():
+class RootCamera():
 
     def __init__(self):
 
-        self.houdinicam = pm.camera(name=savename)
-        self.selection = self.houdinicam[0]
+        self.rootcam = pm.camera(name="rootCam")
+        self.selection = self.rootcam[0]
         self.shape = self.selection.getShape()
 
-    def export_me(self, houdirname, housavename, houext):
+    def __str__(self):
+        return ("{}".format(self.selection))
 
-        # sets the string arguments needed for the ABC export a
-        myfile = str("-file " + houdirname + "/" + housavename + "_houdini" + houext)
-        root = str("-root " + str(self.selection))
-        myframerange = "-frameRange " + str(start) + " " + str(end)
-        exportcommand = myfile + " " + root + " " + myframerange + " " + "-eulerFilter -worldspace"
-        pm.AbcExport(j=exportcommand)
+
+
+class DuplicateCamera():
+
+    def __init__(self,rootcam_selection):
+
+        try:
+            self.mayacam = pm.duplicate(rootcam_selection,name="maya_cam",inputConnections=True)
+            self.selection = self.mayacam[0]
+            self.shape = self.selection.getShape()
+            print("The maya camera has been set")
+        except:
+            pm.confirmDialog(title="Camera Export Error", button="OK",message="There was an error setting the maya camera\nThe camera has not been exported, please try again")
+            exit()
 
     def __str__(self):
         return ("{}".format(self.selection))
 
 
-#SETTING UP THE GLOBAL VARIABLES
 
-start = int(pm.playbackOptions(query=True,min=True))
-end = int(pm.playbackOptions(query=True,max=True))
-scene_scale = 0.1
+
 
 #SETTING THE ABC EXPORT STARTING DIRECTORY
+
 def getexportdir():
     scenedir = pm.sceneName()
     dirname = os.path.dirname(scenedir)
@@ -76,61 +76,64 @@ def getexportdir():
 
 startdir = getexportdir()
 
+# HAVE TO INCLUDE THIS
+def getSceneSettings():
+    """
+    Gets the values for the scenes export
+    :return: tuple (int "start" , int "end", int "scene_scale") 
+    """
+    start = int(pm.playbackOptions(query=True, min=True))
+    end = int(pm.playbackOptions(query=True, max=True))
+
+    return (start, end,)
+
+
 # SETTING THE VARIABLES FOR THE ABC EXPORT
 exportfilepath = pm.fileDialog2(fileFilter="*.abc", dialogStyle=2, startingDirectory=startdir)
 basename = os.path.basename(exportfilepath[0])
-dirname = os.path.dirname(exportfilepath[0])
-savename = os.path.splitext(basename)[0]
-ext = os.path.splitext(basename)[1]
+mydirname = os.path.dirname(exportfilepath[0])
+mysavename = os.path.splitext(basename)[0]
+myext = os.path.splitext(basename)[1]
 
 
-# SETTING THE OBJECTS
-mayacam = MayaCamera()
-hdcam = HoudiniCamera()
+def bakeMe(thecamera,scene_scale=float(1.0)):
+
+    start,end = getSceneSettings()
+
+    for i in range(start, end):
+        # updates the time and gets to camera attributes stored in the origcam object
+        pm.currentTime(i, edit=True, update=True)
+
+        # unpacks the object attributes into vaiables for readability
+        mylens = mayacam.shape.focalLength.get()
+        mysensorx = mayacam.shape.horizontalFilmAperture.get()
+        mysensory = mayacam.shape.verticalFilmAperture.get()
+        mytransx, mytransy, mytransz = pm.xform(mayacam.selection, query=True, ws=True, t=True)
+        myrotx, myroty, myrotz = pm.xform(mayacam.selection, query=True, ws=True, ro=True)
+
+        # key the lens
+        pm.setKeyframe(thecamera.shape, v=mylens, at="focalLength")
+        # key sensor
+        pm.setKeyframe(thecamera.shape, v=mysensorx, at="horizontalFilmAperture")
+        pm.setKeyframe(thecamera.shape, v=mysensory, at="verticalFilmAperture")
+        # key translates and mulitplies it by the scenescale
+        pm.setKeyframe(thecamera.selection, v=mytransx * scene_scale, at="translateX")
+        pm.setKeyframe(thecamera.selection, v=mytransy * scene_scale, at="translateY")
+        pm.setKeyframe(thecamera.selection, v=mytransz * scene_scale, at="translateZ")
+        # key rotates - rotation order ZXY
+        pm.setKeyframe(thecamera.selection, v=myrotz, at="rotateZ")
+        pm.setKeyframe(thecamera.selection, v=myrotx, at="rotateX")
+        pm.setKeyframe(thecamera.selection, v=myroty, at="rotateY")
 
 
-# EXPORTING THE MAYA CAMERA
-if True:
-    try:
-        mayacam.export_me(dirname,savename,ext)
-    except ValueError:
-        print ("There was an error exporting the maya camera")
+def exportCameras():
 
-#BAKING ALL THE ATTRIBUTES FOR HOUDINI
-for i in range(start,end):
-
-    # updates the time and gets to camera attributes stored in the origcam object
-    pm.currentTime(i, edit=True, update=True)
-
-    # unpacks the object attributes into vaiables for readability
-    mylens = mayacam.shape.focalLength.get()
-    mysensorx = mayacam.shape.horizontalFilmAperture.get()
-    mysensory = mayacam.shape.verticalFilmAperture.get()
-    mytransx, mytransy, mytransz = pm.xform(mayacam.selection,query=True,ws=True,t=True)
-    myrotx, myroty, myrotz = pm.xform(mayacam.selection,query=True,ws=True,ro=True)
-
-    # key the lens
-    pm.setKeyframe(hdcam.shape, v=mylens, at="focalLength")
-    # key sensor
-    pm.setKeyframe(hdcam.shape, v=mysensorx, at="horizontalFilmAperture")
-    pm.setKeyframe(hdcam.shape, v=mysensory, at="verticalFilmAperture")
-    # key translates and mulitplies it by the scenescale
-    pm.setKeyframe(hdcam.selection, v=mytransx * scene_scale, at="translateX")
-    pm.setKeyframe(hdcam.selection, v=mytransy * scene_scale, at="translateY")
-    pm.setKeyframe(hdcam.selection, v=mytransz * scene_scale, at="translateZ")
-    # key rotates - rotation order ZXY
-    pm.setKeyframe(hdcam.selection, v=myrotz, at="rotateZ")
-    pm.setKeyframe(hdcam.selection, v=myrotx, at="rotateX")
-    pm.setKeyframe(hdcam.selection, v=myroty, at="rotateY")
+    root_camera = RootCamera()
 
 
-# EXPORT THE ABC FOR HOUDINI AND DELETE THE PLOTTED CAMERA
 
-print(hdcam.selection)
 
-hdcam.export_me(dirname,savename,ext)
-pm.delete(hdcam.selection, s=True)
-
-print("CAMERA SCRIPT HAS ENDED")
+    maya_camera = MayaCamera(root_camera.selection)
+    houdini_camera = HoudiniCamera(root_camera.selection)
 
 
