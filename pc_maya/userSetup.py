44fc1bbf87af6a2c30f57fb24bd14ba27607630b
Roger Wellard <rogerwellard@gmail.com>
2019-08-30 15:40:41 +0200

    updating maya to work on server

--- pc_maya/userSetup.py
+++ pc_maya/userSetup.py
@@ -4,6 +4,7 @@
 import pymel.core as pm
 
 #imports the script and runs the menu command
-pm.evalDeferred("import pc_maya.menus.pc_maya_tools_menu;pc_maya_tools_menu.createMayaMenus()")
+pm.evalDeferred("from pc_maya.menus import pc_maya_tools_menu;pc_maya_tools_menu.createMayaMenus()")
+pm.evalDeferred("from pc_maya.exporters import pc_ABC_camera_exporter")
 
 
