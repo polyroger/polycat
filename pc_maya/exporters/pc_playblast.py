b5b57baf5134f047690ecf3f779c833db5425616
[Roger] <[rogerwellard@gmail.com]>
2019-10-15 09:23:12 +0200

    fixing local dev

--- pc_maya/exporters/pc_playblast.py
+++ pc_maya/exporters/pc_playblast.py
@@ -1,36 +1,9 @@
 import pymel.core as pm
-<<<<<<< HEAD
-=======
 from pipeline_utilities import filenaming
->>>>>>> 6df99589bff6912da484cf221b46ec5e4ec05804
 
 #setting the render settings
 
 
-<<<<<<< HEAD
-pm.deleteUI("window",window=True,panel=True)
-
-#windows are created in a heiracy so the order of these commands are important,
-#when you create a window everything after that becomes a child of the window object
-
-window = pm.window('window')
-form = pm.formLayout()
-editor = pm.modelEditor()
-column = pm.columnLayout('true')
-
-pm.formLayout( form, edit=True, attachForm=[(column, 'top', 0), (column, 'left', 0), (editor, 'top', 0), (editor, 'bottom', 0), (editor, 'right', 0)], attachNone=[(column, 'bottom'), (column, 'right')], attachControl=(editor, 'left', 0, column))
-
-
-camera= pm.ls(selection=True)[0]
-
-#    Attach the camera to the model editor.
-pm.modelEditor( editor, edit=True, camera=camera[0] )
-
-pm.showWindow( window )
-
-#This deletes the window after it has been created
-#pm.deleteUI("window",window=True,panel=True)
-=======
 def getEditor():
     """
     The modelEditor is the node in maya that contains all the information about a modelPanel. A panel is an object in maya that acts as the root of a ui element. The model editor
@@ -44,16 +17,16 @@ def getEditor():
         if pm.modelPanel(panel,query=True,camera=True) == camview:
             modpanel = panel #modpanel is a panel object, so it uses pymel object methods
 
-    print type(modpanel)
+    print (type(modpanel))
              
     cameditorobj =  pm.modelPanel(modpanel,edit=True) #cameditor is a modelPanel object, you pass a panel object to get the modelPanel
-    print type(cameditorobj)
+    print (type(cameditorobj))
     modeditor = pm.modelEditor(cameditorobj,edit=True,activeView=True) #this is a modeleditor object
 
 def saveLocation():
     
     startpath = filenaming.getexportdir("playblast")
-    print startpath
+    print (startpath)
     # savepath = pm.fileDialog2(am=1,ds=2,fm=2,)
  
    
@@ -65,5 +38,4 @@ def runPlayblast():
     
 
   
->>>>>>> 6df99589bff6912da484cf221b46ec5e4ec05804
 
