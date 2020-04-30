14c7a55e0f1f42917edf31494bfd5c3cad130aeb
RogerW <rogerwellard@gmail.com>
2020-04-30 15:59:10 +0200

    added ocio toggle powershell script

---
+++ pipeline_utilities/makeocio.ps1
@@ -0,0 +1,30 @@
+#!/usr/bin/env pwsh.
+
+$myvar = $Env:OCIO
+$on = "Y:\pipeleine\utilities\ocio\aces\OpenColorIO-Configs\aces_1.1\config.ocio"
+$off = ""
+
+
+if ($myvar) {
+    $myvar = $off
+} 
+else {
+    $myvar = $on
+}
+
+[System.Environment]::SetEnvironmentVariable("OCIO",$myvar,[System.EnvironmentVariableTarget]::User)
+
+
+$myvar
+$on
+$off
+
+Clear-Variable -Name "myvar"
+Clear-Variable -Name "on"
+Clear-Variable -Name "off"
+
+$myvar
+$on
+$off
+
+$env:OCIO
