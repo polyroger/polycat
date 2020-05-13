82a2d09fe6507f07957b70e3f0def2de42a152e5
Roger Wellard <rogerwellard@gmail.com>
2020-05-13 16:01:32 +0200

    added houdin pre render scripts

--- pc_houdini/render_scripts/pre_render.py
+++ pc_houdini/render_scripts/pre_render.py
@@ -2,16 +2,21 @@
 
 import hou
 import os
+import re
 
 def saveSimHip():
 
     # from pc_houdini.render_scripts import pre_render;pre_render.saveSimHip() # call this from pre render in houdini
 
+    currenthip = hou.hipFile.path()
+  
     node = hou.node("..")
     fullfilepath = node.parm("file").eval()
     (path,filename) = os.path.split(fullfilepath)
     noext = os.path.splitext(filename)[0]
-    simname = noext + "_simsave.hip"
+    noframe =  re.sub(".\\d+","",noext)
+    ext = ".hip"
+    simname = noframe + "_simsave" + ext
     savefolder = "simsave"
 
     fullsimsavepath = os.path.join(path,savefolder,simname)
@@ -20,6 +25,7 @@ def saveSimHip():
     if not os.path.exists(path):
         os.makedirs(path)
     
-    hou.hipFile.save(file_name=fullsimsavepath,save_to_recent_files=True)
+    hou.hipFile.save(file_name=fullsimsavepath,save_to_recent_files=False)
+    hou.hipFile.setName(currenthip)
     
 
