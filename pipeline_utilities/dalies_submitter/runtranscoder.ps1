901d02797446dda2cb5aad918f0ba784320c62db
Roger Wellard <rogerwellard@gmail.com>
2020-07-27 14:50:12 +0200

    updading transcoder shortcut to run in anaconda shell

--- pipeline_utilities/dalies_submitter/runtranscoder.ps1
+++ pipeline_utilities/dalies_submitter/runtranscoder.ps1
@@ -3,8 +3,4 @@
 $transcoder = "\\YARN\projects\pipeline\development\pipeline_utilities\dalies_submitter\pc_dailies_ui.py"
 $conaenv = "\\YARN\projects\pipeline\utilities\conda_envs\polycat_3_production"
 
-Unblock-File -Path $transcoder
-
-conda activate $conaenv
-
-python $transcoder
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate $conaenv ; python $transcoder "
