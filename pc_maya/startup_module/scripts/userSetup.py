85cdfc7d8c1ccee406c2e279346b8c8173bf39cf
Roger Wellard <rogerwellard@gmail.com>
2020-07-31 10:27:45 +0200

    added pc_maya import

--- pc_maya/startup_module/scripts/userSetup.py
+++ pc_maya/startup_module/scripts/userSetup.py
@@ -4,20 +4,21 @@
 import pymel.core as pm
 import os
 import sys
+import pc_maya
 
 
-#package dependencies, not sure if this is the best way to do this.
+# package dependencies, not sure if this is the best way to do this.
 # sys.path.append("F:\\projects\\pipeline\\packages") # for remote use
 sys.path.append("\\\\YARN\\projects\\pipeline\\packages")
 
-#external packages, after path append
+# #external packages, after path append
 import scandir
 import pullframerange
 
-# these are deffered because they are required for the menu creation, if you are adding to the tools menu they must be added as deffered
+# # these are deffered because they are required for the menu creation, if you are adding to the tools menu they must be added as deffered
 pm.evalDeferred("from pc_maya.menus import pc_maya_tools_menu;pc_maya_tools_menu.createMayaMenus()")
 
-# Checking seqence data
+# # Checking sequence data
 pm.evalDeferred("pullframerange.makeRange()")
 
 
