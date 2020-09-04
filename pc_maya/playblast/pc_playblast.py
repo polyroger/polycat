4d322936925139ab781d65e5d02fcd76337bb044
Roger Wellard <rogerwellard@gmail.com>
2020-09-04 15:02:02 +0200

    added top and bottom color set camera display options

--- pc_maya/playblast/pc_playblast.py
+++ pc_maya/playblast/pc_playblast.py
@@ -11,6 +11,8 @@ def setTempGlobals():
     pm.setAttr("hardwareRenderingGlobals.multiSampleEnable",1)
     pm.setAttr("hardwareRenderingGlobals.aasc",16)
     pm.displayRGBColor("background",0.2,0.2,0.2)
+    pm.displayRGBColor("backgroundTop",0.2,0.2,0.2)
+    pm.displayRGBColor("backgroundBottom",0.2,0.2,0.2)
 
     return "Temporarily set global settings"
 
@@ -34,10 +36,17 @@ def createPBWindow(windowname,camera):
                     lights=False,
                     joints=False,
                     locators=False,
+                    pivots=False,
                     nurbsCurves=False,
                     displayAppearance="smoothShaded")
+
+    camera.setDisplayFilmGate(False)
+    camera.setDisplayGateMask(False)
+    camera.setFilmFit("fillFilmFit")
+    camera.setOverscan(1.0)
+    # print(camshape.getDisplayGateMask())
     
-    # pbwindow.show()
+    pbwindow.show()
     pm.setFocus(blastpanel)
 
     return windowname
@@ -55,7 +64,8 @@ def runPlayblast(savepath,start,end,rglobals):
                         showOrnaments=False,
                         viewer=False,
                         width=rglobals["resx"],
-                        height=rglobals["resy"])
+                        height=rglobals["resy"],
+                        offScreen=True)
         
         print("Playblast completed")
         return True
@@ -69,7 +79,10 @@ def runPlayblast(savepath,start,end,rglobals):
 def cleanUp(rglobals,windowname):
     
     pm.deleteUI(windowname)
+    
     pm.displayRGBColor("background",float(rglobals["bgrgb"][0]),float(rglobals["bgrgb"][1]),float(rglobals["bgrgb"][2]))
+    pm.displayRGBColor("backgroundTop",float(rglobals["bgtoprgb"][0]),float(rglobals["bgtoprgb"][1]),float(rglobals["bgtoprgb"][2]))
+    pm.displayRGBColor("backgroundBottom",float(rglobals["bgbottomrgb"][0]),float(rglobals["bgbottomrgb"][1]),float(rglobals["bgbottomrgb"][2]))
     pm.setAttr("hardwareRenderingGlobals.multiSampleEnable",0)
     pm.setAttr("hardwareRenderingGlobals.aasc",8)
 
