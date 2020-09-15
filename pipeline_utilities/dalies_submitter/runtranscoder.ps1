8a603f07320df2a1199b2b8ecd654e894a8de70d
Roger Wellard <rogerwellard@gmail.com>
2020-09-15 11:50:22 +0200

    fixing gitignore

--- pipeline_utilities/dalies_submitter/runtranscoder.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-
-$transcoder = "\\YARN\projects\pipeline\development\pipeline_utilities\dalies_submitter\pc_dailies_ui.py"
-$conaenv = "\\YARN\projects\pipeline\environments\polycat3"
-
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& 'C:\ProgramData\Anaconda3\shell\condabin\conda-hook.ps1' ; conda activate $conaenv ; python $transcoder "
