5e1440e25e558dd73a719aa05e377e4fe85e9621
Roger Wellard <rogerwellard@gmail.com>
2020-10-19 11:12:08 +0200

    initial commit of the the logging clas

---
+++ pipeline_utilities/pc_logging/houdini_logger.py
@@ -0,0 +1,15 @@
+"""
+This is just a houdini subclass of the Pc_Logger, just so if there needs to be any
+houdini specific changes they wont effect other apps.
+
+"""
+
+
+from pc_logging import Pc_Logger
+
+class Houdini_Logger(Pc_Logger):
+
+    LOGGER_NAME = "houdini_logger"
+
+    PROPAGATE = True
+
---
+++ pipeline_utilities/pc_logging/maya_logger.py
@@ -0,0 +1,15 @@
+"""
+This is just a maya subclass of the Pc_Logger, just so if there needs to be any
+maya specific changes they wont effect other apps.
+
+"""
+
+
+from pc_logging import Pc_Logger
+
+class Maya_Logger(Pc_Logger):
+
+    LOGGER_NAME = "maya_logger"
+
+    PROPAGATE = True
+
